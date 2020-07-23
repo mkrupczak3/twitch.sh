d063a481785f3eadb5dcefacfaabe8724a175170
mkrupczak3 <matthew@krupczak.org>
2020-07-23 18:58:48 -0400

    Create twitch.sh

---
+++ twitch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+# (This is not very hard)
+# Usage:
+# root@pentagon:~$ sh twitch.sh
+#
+firefox https://dashboard.twitch.tv/u/t3l3tubie/stream-manager &
+firefox https://www.twitch.tv/t3l3tubie &
